e1740f9b66bfd2d4fed417bfe8937c22e79dca4c
zeraati <zeraatimail@gmail.com>
2024-07-28 19:07:44 +0330

    TimeOnlyExtension

---
+++ Common/Extenstion/TimeOnlyExtension.cs
@@ -0,0 +1,6 @@
+﻿namespace Common;
+public static class TimeOnlyExtension
+{
+    public static TimeOnly TehranToUtc(this TimeOnly input) => input.Add(-new TimeSpan(3, 30, 0));
+    public static TimeOnly UtcToTehran(this TimeOnly input) => input.Add(new TimeSpan(3, 30, 0));
+}
--- Common/Extenstion/TimeSpan.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Common;
-public static class TimeSpanExtension
-{
-    public static TimeSpan TehranToUtc(this TimeSpan input) => input.Add(-new TimeSpan(3, 30, 0));
-    public static TimeSpan UtcToTehran(this TimeSpan input) => input.Add(new TimeSpan(3, 30, 0));
-}
