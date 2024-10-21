abacc7be3b6f037a1bc37908e1f0f4cc8ce9e55b
vigbe32mti9 <vigbe32m@ti9.ir>
2024-10-21 23:20:57 +0330

    *

--- Common/Extenstion/String.cs
+++ Common/Extenstion/String.cs
@@ -1,8 +1,10 @@
-﻿namespace Common;
+﻿using System.Text;
+
+namespace Common;
 public static class StringExtension
 {
     public static string Bold(this string input) => $"* {input} *";
-    public static string NewLineBefore(this string input,int count=1)
+    public static string NewLineBefore(this string input, int count = 1)
     {
         for (int i = 0; i < count; i++) input = "\n" + input;
         return input;
@@ -11,5 +13,24 @@ public static class StringExtension
     {
         for (int i = 0; i < count; i++) input += "\n";
         return input;
+    }
+
+    public static string CamelCaseToKebabCase(this string input)
+    {
+        var builder = new StringBuilder();
+        bool caseFlag = false;
+
+        for (int i = 0; i < input.Length; ++i)
+        {
+            char c = input[i];
+            if (c == '-') caseFlag = true;
+            else if (caseFlag)
+            {
+                builder.Append(char.ToUpper(c));
+                caseFlag = false;
+            }
+            else builder.Append(char.ToLower(c));
+        }
+        return builder.ToString();
     }
 }
