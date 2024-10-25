069af665739eb0c5ef50f80fbaa1b3fbc88c664f
zeraati <zeraatimail@gmail.com>
2024-10-25 23:05:54 +0330

    OrderReceived

--- CommonBot/ButtonEnum.cs
+++ CommonBot/ButtonEnum.cs
@@ -4,7 +4,7 @@ using Telegram.Bot;
 namespace NozarfBot;
 public static class ButtonEnum
 {
-    public static readonly ButtonEnumItem PlaceOrder = new("ثبت سفارش", nameof(PlaceOrder).CamelCaseToKebabCase());
+    public static readonly ButtonEnumItem PlaceOrder = new("ثبت / ویرایش سفارش", nameof(PlaceOrder).CamelCaseToKebabCase());
     public static readonly ButtonEnumItem SendOrder = new("ارسال سفارش", nameof(SendOrder).CamelCaseToKebabCase());
     public static readonly ButtonEnumItem MakePayment = new("پرداخت هزینه سفارش", nameof(MakePayment).CamelCaseToKebabCase());
 }
--- CommonBot/ClientExtention/SaveDocument.cs
+++ CommonBot/ClientExtention/SaveDocument.cs
@@ -8,14 +8,14 @@ public static partial class ClientExtention
         var fileId = message.Document!.FileId;
         var fileInfo = await bot.GetFileAsync(fileId);
 
-        var fileName = Guid.NewGuid() + Path.GetExtension(message.Document.FileName);
+        var fileName = fileId + Path.GetExtension(message.Document.FileName);
         var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Document", fileName);
         await using (var fileStream = new FileStream(filePath, FileMode.Create))
         {
             await bot.DownloadFileAsync(fileInfo.FilePath!, fileStream);
         }
 
-        return filePath;
+        return fileId;
     }
 }
 
