68d9195c1207f33a76a971226397c22655ccbc42
zeraati <zeraatimail@gmail.com>
2024-10-26 20:11:48 +0330

    icommand

--- Common/CQRS/IRequestValidator.cs
+++ Common/CQRS/IRequestValidator.cs
@@ -1,4 +1,9 @@
 ﻿namespace MediatR;
+public interface IRequestValidator<TRequest> where TRequest : IRequest
+{
+    Task Validate(TRequest request, CancellationToken cancellation);
+}
+
 public interface IRequestValidator<TRequest, TResult> where TRequest : IRequest<TResult>
 {
     Task Validate(TRequest request,CancellationToken cancellation);
--- Common/Common/ApiResult.cs
+++ Common/Common/ApiResult.cs
@@ -11,19 +11,32 @@ public class ApiResult : ActionResult
     public string? Message { get; }
 }
 
-public class ApiResultPaging<TData> : ApiResult
+public class ApiResult<TData> : ApiResult
 {
-    public ApiResultPaging(TData data, Paging paging, int totalCount,bool success = true, string? message = null)
-        : base(success, message)
+    public ApiResult(TData data, bool success = true, string? message = null) : base(success, message)
     {
-        PageSize = paging.PageSize;
-        PageNumber = paging.PageNumber;
-        TotalCount = totalCount;
-        TotalPages = (int)Math.Ceiling((double)TotalCount / PageSize);
+        Data = data;
     }
 
-    public int PageSize { get; }
-    public int PageNumber { get; }
-    public int TotalCount { get; }
-    public int TotalPages { get; }
+    public TData? Data { get; }
+}
+
+public class ApiResultPaging<TData> : ApiResult<TData>
+{
+    public ApiResultPaging(TData data, Paging paging, int totalCount, bool success = true, string? message = null)
+        : base(data,success, message)
+    {
+        Pagination = new(paging, totalCount);
+    }
+
+    public Pagination Pagination { get; set; }
+    
+}
+
+public class Pagination(Paging paging, int totalCount)
+{
+    public int PageSize { get; }=paging.PageSize;
+    public int PageNumber { get; } = paging.PageNumber;
+    public int TotalCount { get; }=totalCount;
+    public int TotalPages { get; }= (int)Math.Ceiling((double)totalCount / paging.PageSize);
 }
