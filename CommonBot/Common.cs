2a09dc4dc4d5adc74c0ffc073a1904ac4756c983
zeraati <zeraatimail@gmail.com>
2024-10-27 22:56:59 +0330

    *

--- CommonBot/Common.cs
+++ CommonBot/Common.cs
@@ -3,11 +3,15 @@
 namespace NozarfBot;
 public class Common
 {
-    public static void SetBot(ref TelegramBotClient bot, TelegramBotClient.OnMessageHandler messageHandler, TelegramBotClient.OnUpdateHandler updateHandler)
+    public static void SetBot(
+        ref TelegramBotClient bot, 
+        string token,
+        TelegramBotClient.OnMessageHandler messageHandler,
+        TelegramBotClient.OnUpdateHandler updateHandler)
     {
         if (bot == null)
         {
-            var option = new TelegramBotClientOptions("327072116:q5XWiPLYkQMQlhxuCiKz13kYNVzzdssfZ6tLSySM", "https://tapi.bale.ai/");
+            var option = new TelegramBotClientOptions(token, "https://tapi.bale.ai/");
             bot = new TelegramBotClient(option);
         }
 
