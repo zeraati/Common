7baf95e386c8be2a0efe79230b586e346d7022ff
zeraati <zeraatimail@gmail.com>
2024-10-29 22:47:22 +0330

    *

--- Common/CQRS/ICommand.cs
+++ Common/CQRS/ICommand.cs
@@ -1,8 +1,8 @@
 ﻿namespace MediatR;
 public interface ICommand : IRequest;
-public interface ICommand<out TResult> : IRequest<TResult>, ICommand;
-
 public interface ICommandHandler<TCommand>: IRequestHandler<TCommand> where TCommand : ICommand;
+
+public interface ICommand<out TResult> : IRequest<TResult>;
 public interface ICommandHandler<TCommand, TResult>: IRequestHandler<TCommand, TResult> where TCommand : ICommand<TResult>;
 
 public record CreateCommandResult(long Id);
