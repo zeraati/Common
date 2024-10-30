f224d0e6e605e7256d2ccda5c2b5091fc0082d69
zeraati <zeraati@csis.local>
2024-10-30 08:53:45 +0330

    *

--- Common/Extenstion/DbContextBuilderExtension.cs
+++ Common/Extenstion/DbContextBuilderExtension.cs
@@ -1,16 +1,16 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Extensions.DependencyInjection;
 public static class DbContextBuilderExtension
 {
-    public static WebApplicationBuilder AddDbContext<TContext>(this WebApplicationBuilder builder, bool localForLinux=false) where TContext : DbContext
+    public static WebApplicationBuilder AddDbContext<TContext>(this WebApplicationBuilder builder, bool locallyInReleaseMode = false) where TContext : DbContext
     {
         var connection = builder.Configuration.GetConnectionString("SqlServer")!;
-        if (localForLinux && RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        if (locallyInReleaseMode && !Debugger.IsAttached)
         {
             connection = Regex.Replace(connection, @"Server=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3};", "Server=.;");
         }
