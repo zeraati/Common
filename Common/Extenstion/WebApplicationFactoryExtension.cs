01919cae1b600b4de05d0add86938f40b2fdc41e
zeraati <zeraatimail@gmail.com>
2024-10-31 00:32:07 +0330

    *

---
+++ Common/Extenstion/WebApplicationFactoryExtension.cs
@@ -0,0 +1,16 @@
+﻿using Common;
+using System.Net.Http.Json;
+
+namespace Microsoft.AspNetCore.Mvc.Testing;
+public static class WebApplicationFactoryExtension
+{
+    public static async Task<TResult> SendRequest<TProgram, TResult>(this WebApplicationFactory<TProgram> factory, TestClientRequest request)
+        where TProgram : class
+    {
+        var httpRequest = request.Create();
+        var response = await factory.CreateClient().SendAsync(httpRequest);
+        var result = await response.Content.ReadFromJsonAsync<TResult>();
+
+        return result!;
+    }
+}
---
+++ Common/Util/Test.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using System.Text;
+using NUnit.Framework;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Common;
+public static partial class Test
+{
+    public static async Task<TResult> TestCommandQuery<TProgram, TResult>(string url, HttpMethod method, IBaseRequest? baseRequest = null)
+            where TProgram : class
+            where TResult : ApiResult
+    {
+        var factory = new WebApplicationFactory<TProgram>().WithWebHostBuilder(x => { });
+        var request = new TestClientRequest(url, method, baseRequest);
+        var result = await factory.SendRequest<TProgram,TResult>(request);
+        Assert.Equals(result.Success,true);
+        return result;
+    }
+}
+
+public class TestClientRequest
+{
+    public TestClientRequest(string url, HttpMethod method, object? content = null)
+    {
+        Url = url;
+        Method = method;
+        Content = content;
+    }
+
+    public string Url { get; }
+    public HttpMethod Method { get; }
+    public object? Content { get; }
+
+    public HttpRequestMessage Create()
+    {
+        var httpRequest = new HttpRequestMessage(Method, Url);
+        if ((Method == HttpMethod.Post || Method == HttpMethod.Put) && Content != null) httpRequest.Content = ToStringContent(Content);
+        return httpRequest;
+    }
+
+    private StringContent ToStringContent(object obj)
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            MaxDepth = 16,
+            ReferenceHandler = ReferenceHandler.IgnoreCycles
+        };
+        return new StringContent(JsonSerializer.Serialize(obj, options), Encoding.UTF8, "application/json");
+    }
+}
+
