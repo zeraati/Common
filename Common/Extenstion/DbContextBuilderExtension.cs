4ad8a1dafc09fe17d8b8c0dbcb5d1ce33e67afd3
zeraati <zeraati@csis.local>
2024-10-31 08:23:41 +0330

    DbContextBuilderExtension

--- Common/Extenstion/DbContextBuilderExtension.cs
+++ Common/Extenstion/DbContextBuilderExtension.cs
@@ -1,24 +1,13 @@
-﻿using Common;
-using System.Diagnostics;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Extensions.DependencyInjection;
 public static class DbContextBuilderExtension
 {
-    public static WebApplicationBuilder AddDbContext<TContext>(this WebApplicationBuilder builder, bool locallyInReleaseMode = false) where TContext : DbContext
+    public static WebApplicationBuilder AddDbContext<TContext>(this WebApplicationBuilder builder) where TContext : DbContext
     {
         var connection = builder.Configuration.GetConnectionString("SqlServer")!;
-        if (locallyInReleaseMode && !Debugger.IsAttached)
-        {
-            connection = Regex.Replace(connection, @"Server=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3};", "Server=.;");
-            connection.Replace("Server=.;", "host.docker.internal");
-            //TODO
-            //if (Util.OsIsDocker)connection.Replace("Server=.;", "host.docker.internal");
-        }
-
         builder.Services.AddDbContext<TContext>(options => options.UseSqlServer(connection));
         return builder;
     }
