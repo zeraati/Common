d8420d06dd9c4c481215336a69815a7ef0082605
zeraati <zeraati@csis.local>
2024-11-03 12:17:45 +0330

    *

--- Common/Common/OrderAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Common;
-[AttributeUsage(AttributeTargets.Class, Inherited = false)]
-public class OrderAttribute : Attribute
-{
-    public int Order { get; }
-    public OrderAttribute(int order)=> Order = order;
-}
