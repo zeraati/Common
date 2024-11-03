4156d5e9afa741b03ee48bd1626fa2ff2540311f
zeraati <zeraatimail@gmail.com>
2024-11-03 18:28:57 +0330

    IHttpContextAccessorExtension

---
+++ Common/Extenstion/IHttpContextAccessorExtension.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Common;
+public static class IHttpContextAccessorExtension
+{
+    public static Guid? GetTraceId(this IHttpContextAccessor accessor) 
+        => Guid.Parse(accessor.HttpContext?.TraceIdentifier ?? "");
+}
+
