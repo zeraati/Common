0f8cd47504666c96fc10625536bcf178267d5d8b
zeraati <zeraati@csis.local>
2024-11-03 11:17:17 +0330

    *

--- Common/Common/LogRequestMiddleware.cs
+++ Common/Common/LogRequestMiddleware.cs
@@ -21,13 +21,8 @@ public class LogRequestMiddleware
         {
             context.Request.EnableBuffering();
             var request = context.Request;
-            var logRequest = new
-            {
-                Route = $"({request.Method}){request.Scheme}://{request.Host}{request.Path}",
-                Authorization = request.Headers.Authorization.ToString(),
-                Body = Json.AddDepthToJson(await new StreamReader(request.Body).ReadToEndAsync()),
-                Date=DateTime.UtcNow
-            };
+            var requestBodyLog = await new StreamReader(request.Body).ReadToEndAsync();
+            var logRequest = new RequestLog(request, requestBodyLog);
             _logger.LogDebug(logRequest.ToJson());
 
             context.Request.Body.Position = 0;
@@ -39,13 +34,8 @@ public class LogRequestMiddleware
 
             context.Response.Body.Seek(0, SeekOrigin.Begin);
 
-            var logResponse = new
-            {
-                context.Response.StatusCode,
-                Body = Json.AddDepthToJson(await new StreamReader(context.Response.Body).ReadToEndAsync()),
-                Date = DateTime.UtcNow,
-                Duration = (DateTime.UtcNow-logRequest.Date).Seconds+ " seconds"
-            };
+            var responseBodyLog= await new StreamReader(context.Response.Body).ReadToEndAsync();
+            var logResponse = new ResponseLog(context.Response, responseBodyLog, logRequest.Date);
             _logger.LogDebug(logResponse.ToJson());
 
             context.Response.Body.Seek(0, SeekOrigin.Begin);
@@ -53,4 +43,38 @@ public class LogRequestMiddleware
         }
         else await _next(context);
     }
+}
+
+#pragma warning disable CS8618
+public class RequestLog
+{
+    public RequestLog(HttpRequest request,string body)
+    {
+        Url = $"({request.Method}){request.Scheme}://{request.Host}{request.Path}";
+        Authorization = request.Headers.Authorization.ToString();
+        Body = Json.AddDepthToJson(body);
+    }
+
+    public string Url { get; }
+    public string Authorization { get; }
+    public string Body { get; }
+    public DateTime Date { get; }= DateTime.UtcNow;
+}
+
+public class ResponseLog
+{
+    public ResponseLog(HttpResponse response, string body,DateTime requestDate)
+    {
+        var now=DateTime.UtcNow;
+
+        StatusCode = response.StatusCode;
+        Body = Json.AddDepthToJson(body);
+        Date = now;
+        Duration = (now - requestDate).Seconds + " seconds";
+    }
+
+    public int StatusCode { get;}
+    public string Body { get; }
+    public DateTime Date { get; }
+    public string Duration { get; }
 }
