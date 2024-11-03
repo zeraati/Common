445b8a597917deb16599d52f8f6fa5c34557e240
zeraati <zeraatimail@gmail.com>
2024-11-03 18:32:16 +0330

    *

--- Common/Filter/ExceptionFilter.cs
+++ Common/Filter/ExceptionFilter.cs
@@ -1,11 +1,17 @@
 ﻿using Common;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Mvc.Filters;
 public class ExceptionFilter : IExceptionFilter
 {
     private readonly ILogger<ExceptionFilter> _logger;
-    public ExceptionFilter(ILogger<ExceptionFilter> logger) => _logger = logger;
+    private readonly IHttpContextAccessor _contextAccessor;
+    public ExceptionFilter(ILogger<ExceptionFilter> logger, IHttpContextAccessor contextAccessor)
+    {
+        _logger = logger;
+        _contextAccessor = contextAccessor;
+    }
 
     public void OnException(ExceptionContext context)
     {
@@ -19,7 +25,7 @@ public class ExceptionFilter : IExceptionFilter
             var result = new ApiResult(success: false, message: MessageResource.UnmanagedException);
             context.Result = new BadRequestObjectResult(result);
             var descriptor = context.ActionDescriptor.DisplayName!;
-            _logger.LogErrorCustom(traceId:null,context.Exception, descriptor: descriptor);
+            _logger.LogErrorCustom(_contextAccessor.GetTraceId(),context.Exception, descriptor: descriptor);
         }
     }
 }
