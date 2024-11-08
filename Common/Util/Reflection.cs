851a83b8e3cdb73dece1d9600e0be817e7454db8
zeraati <zeraatimail@gmail.com>
2024-11-08 20:20:07 +0330

    *

---
+++ Common/Util/Reflection.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace Common;
+public static class Reflection
+{
+    public static string CallerThisMethod()
+    {
+        var stackTrace = new StackTrace();
+        var frame = stackTrace.GetFrame(2);
+        var method = frame!.GetMethod()!;
+
+        return method.Name;
+    }
+}
+
