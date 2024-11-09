d20966e19a37a452bb683eeaa027af412e7e54d2
zeraati <zeraatimail@gmail.com>
2024-11-09 17:39:39 +0330

    *

--- Common/Extenstion/IListExtension.cs
+++ Common/Extenstion/IListExtension.cs
@@ -6,4 +6,14 @@ public static class IListExtension
         for (var i = 0; i < source.Count(); i++) action(source[i]);
         return source;
     }
+
+    public static ICollection<TSource> ForEach<TSource>(this ICollection<TSource> source, Action<TSource> action)
+    {
+        for (var i = 0; i < source.Count(); i++) {
+
+            var item = source.ElementAt(i);
+            action(item);
+        }
+        return source;
+    }
 }
