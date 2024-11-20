24ae124d1a59af91d71616ff96f4773264e048e4
zeraati <zeraatimail@gmail.com>
2024-11-21 01:10:57 +0330

    *

--- CommonBot/ClientExtention/SaveDocument.cs
+++ CommonBot/ClientExtention/SaveDocument.cs
@@ -6,13 +6,13 @@ public static partial class ClientExtention
     public static async Task<string> SaveDocument(this ITelegramBotClient bot, Message message)
     {
         var fileId = message.Document!.FileId;
-        var fileInfo = await bot.GetFileAsync(fileId);
+        var fileInfo = await bot.GetFile(fileId);
 
         var fileName = fileId + Path.GetExtension(message.Document.FileName);
         var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Document", fileName);
         await using (var fileStream = new FileStream(filePath, FileMode.Create))
         {
-            await bot.DownloadFileAsync(fileInfo.FilePath!, fileStream);
+            await bot.DownloadFile(fileInfo.FilePath!, fileStream);
         }
 
         return fileId;
